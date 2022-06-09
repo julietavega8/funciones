d044a79d395ba0f7bf244b768aeb3b172ca3028e
julietavega8 <85358049+julietavega8@users.noreply.github.com>
2022-06-09 08:44:21 -0300

    Add files via upload

---
+++ EEE/apps.js
@@ -0,0 +1,26 @@
+// sumar dos numeros 
+
+let numerito = parseInt(prompt("Ingrese numero"))
+let numerito2 = parseInt(prompt("Ingrese otro numero"))
+let resultado = numerito + numerito2 
+document.write(resultado)
+
+
+
+//forma corta 
+
+//function suma(num1, num2){
+    //return num1 + num2
+//}
+
+//suma(10, 5 )
+
+
+let nombre= prompt("Ingrese nombre")
+console.log(nombre)
+
+//funcion flecha 
+
+let a = 4;
+let b = 2;
+() => a + b + 100;
